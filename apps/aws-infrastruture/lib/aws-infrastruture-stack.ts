3aa41eae0aa098d3f92b673e3aac170d6819674c
Jeffthe Pierre <jeffthep@gmail.com>
2021-05-20 20:50:55 -0400

    updated structure of cdk test stack

--- apps/aws-infrastruture/lib/aws-infrastruture-stack.ts
+++ apps/aws-infrastruture/lib/aws-infrastruture-stack.ts
@@ -1,9 +1,18 @@
+import * as sns from '@aws-cdk/aws-sns';
+import * as subs from '@aws-cdk/aws-sns-subscriptions';
+import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
 
 export class AwsInfrastrutureStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const queue = new sqs.Queue(this, 'TempQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300),
+    });
+
+    const topic = new sns.Topic(this, 'TempTopic');
+
+    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
