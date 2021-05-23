b2a4d192be9b270661c49ee5f2abd474d9c67396
Jeffthe Pierre <jeffthep@gmail.com>
2021-05-23 03:17:54 -0400

    added specs for dynamo tables

--- apps/aws-infrastruture/lib/dynamo/meta-dynamo/metadata-dynamo.ts
+++ apps/aws-infrastruture/lib/dynamo/meta-dynamo/metadata-dynamo.ts
@@ -1,10 +1,19 @@
-import { Table, TableProps } from '@aws-cdk/aws-dynamodb';
+import { AttributeType, Table, TableProps } from '@aws-cdk/aws-dynamodb';
 import { Construct } from '@aws-cdk/core';
 
+const TABLE_NAME = 'GeoCacheFileMeta';
 export class GeoCacheMetadataDynamo extends Table {
-  constructor(scope: Construct) {
-    const id: string = '';
-    const props: TableProps = null;
+  constructor(
+    scope: Construct,
+    id: string = TABLE_NAME,
+    props: TableProps = {
+      tableName: TABLE_NAME,
+      partitionKey: {
+        name: 'postId',
+        type: AttributeType.STRING,
+      },
+    }
+  ) {
     super(scope, id, props);
   }
 }
--- apps/aws-infrastruture/lib/stacks/dynamo-stack.ts
+++ apps/aws-infrastruture/lib/stacks/dynamo-stack.ts
@@ -1,7 +1,15 @@
+import { Table } from '@aws-cdk/aws-dynamodb';
 import { Construct, Stack, StackProps } from '@aws-cdk/core';
+import { GeoCacheMetadataDynamo, GeoCacheUserDynamoDB } from '../dynamo';
 
 export class GeoCacheDynamoStack extends Stack {
+  userTable: Table;
+  postMetaDataTable: Table;
+
   constructor(scope?: Construct, id?: string, props?: StackProps) {
     super(scope, id, props);
+
+    this.userTable = new GeoCacheUserDynamoDB(this);
+    this.postMetaDataTable = new GeoCacheMetadataDynamo(this);
   }
 }
